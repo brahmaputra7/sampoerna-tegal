44f71d2e9793519143ff983d97799fb6d6285bde
Brahma Putra <brahmaputra1996@gmail.com>
2022-10-29 16:15:24 +0700

    Test

--- pages/index.vue
+++ pages/index.vue
@@ -16,7 +16,7 @@
             </v-col>
             <v-col cols="12" md="6">
               asd
-              <img src="img/title.png" width="100%"  data-aos="flip-left"/>
+              <img src="img/title.png" width="50%"  data-aos="flip-left"/>
             </v-col>
           </v-row>
         </v-container>
